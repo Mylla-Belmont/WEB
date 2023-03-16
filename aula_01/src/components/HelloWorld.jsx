079e9b6365ef455cc42f4ea2de227b6a7abe91c5
Camila Belmont <camilabelmont.alu@gmail.com>
2023-03-16 18:35:21 -0300

    update

--- aula_01/src/components/HelloWorld.jsx
+++ aula_01/src/components/HelloWorld.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+// não é html, e jsx. Não tem como colocar html dentro de 
+// um bloco de codigos javaScript
 function HelloWolrd() {
   return (
     <div>
-      <h1>Coisa</h1>
+      <h1>Coisa</h1> 
     </div>
   )
 }
