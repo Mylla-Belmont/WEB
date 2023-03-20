d438acc2e3ed5548175f40020c8d1906d3e78501
Camila Belmont <camilabelmont.alu@gmail.com>
2023-03-20 08:45:47 -0300

    update

--- aula_01/src/App.js
+++ aula_01/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 //import HelloWorld from "./components/HelloWorld_00";
 //import Calculadora_01 from './components/Calculadora_01';
-import MyProps from './components/MyProps_02';
+//import MyProps from './components/MyProps_02';
+//import MeusDados from './atividade00/MeusDados_01';
+import Props from './atividade00/MeusDados_02';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MyProps 
-          nome="Belmont"
-          idade={21}
-          curso="Si"
+        {/* <MeusDados/> */}
+        <Props
+          nome="Camila Belmont Mesquita"
+          curso="Sistemas de Informação"
+          universidade="Universidade Federal do Ceará"
         />
       </header>
     </div>
